6ee78fead4fe71137f9336b7e316f78a3eca7443
Patryk Konopka <skinex11@gmail.com>
2019-05-07 18:08:09 +0200

    Dodawanie ścieżki przez plik

--- Szyfrator/Gui.py
+++ Szyfrator/Gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.filedialog import askopenfilename
 
 
 def gui():
@@ -7,10 +8,16 @@ def gui():
             Frame.__init__(self, master)
             self.master = master
             self.input = StringVar()
+            self.pathInput = Entry(self)
             self.init_window()
+
+        def onBrowse(self):
+            path = askopenfilename()
+            self.pathInput.insert(0, path)
 
         def onEncrypt(self, input):
             path = input.get()
+            print(path)
 
 
         def  init_window(self):
@@ -21,11 +28,11 @@ def gui():
             self.pack(fill=BOTH, expand=1)
 
             #tworzenie text input
-            pathInput = Entry(self, textvariable=self.input)
-            pathInput.pack()
+            self.pathInput.configure(textvariable=self.input)
+            self.pathInput.pack()
 
             #tworzenie buttona. Trzeba dodać comand=encrypt()
-            browseButton = Button(self, text="Przeglądaj")
+            browseButton = Button(self, text="Przeglądaj", command=self.onBrowse)
             encryptButton = Button(self, text="Encrypt", command=lambda: self.onEncrypt(self.input))
             decryptButton = Button(self, text="Decrypt")
 
@@ -33,7 +40,7 @@ def gui():
             browseButton.place(x=150, y=50)
             encryptButton.place(x=55, y=100)
             decryptButton.place(x=135, y=100)
-            pathInput.place(x=10, y=55)
+            self.pathInput.place(x=10, y=55)
 
     root = Tk()
     #rozmiar okna
