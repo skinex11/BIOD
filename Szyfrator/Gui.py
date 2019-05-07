434c8b3727fe75506f10faac224f1c4b54d83389
Patryk Konopka <skinex11@gmail.com>
2019-05-07 17:54:13 +0200

    Odczytywanie z text input

--- Szyfrator/Gui.py
+++ Szyfrator/Gui.py
@@ -6,19 +6,29 @@ def gui():
         def __init__(self, master=None):
             Frame.__init__(self, master)
             self.master = master
+            self.input = StringVar()
             self.init_window()
+
+        def onEncrypt(self, input):
+            path = input.get()
+
 
         def  init_window(self):
             #tytuł okna
             self.master.title("Encryptor BIOD")
+
             #widget zajmuje całe okno
             self.pack(fill=BOTH, expand=1)
+
+            #tworzenie text input
+            pathInput = Entry(self, textvariable=self.input)
+            pathInput.pack()
+
             #tworzenie buttona. Trzeba dodać comand=encrypt()
             browseButton = Button(self, text="Przeglądaj")
-            encryptButton = Button(self, text="Encrypt")
+            encryptButton = Button(self, text="Encrypt", command=lambda: self.onEncrypt(self.input))
             decryptButton = Button(self, text="Decrypt")
-            #tworzenie text input
-            pathInput = Entry(self)
+
             #umiejscowienie buttona
             browseButton.place(x=150, y=50)
             encryptButton.place(x=55, y=100)
