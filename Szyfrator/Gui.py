159fce3552bd7bf60f2822c00a50eb57c6bc9e88
Patryk Konopka <skinex11@gmail.com>
2019-05-07 18:21:02 +0200

    Domyślne wartości entry

--- Szyfrator/Gui.py
+++ Szyfrator/Gui.py
@@ -7,17 +7,19 @@ def gui():
         def __init__(self, master=None):
             Frame.__init__(self, master)
             self.master = master
-            self.input = StringVar()
+            self.inputPath = StringVar()
+            self.inputKey = StringVar()
             self.pathInput = Entry(self)
+            self.keyInput = Entry(self)
             self.init_window()
 
         def onBrowse(self):
             path = askopenfilename()
+            self.pathInput.delete(0, 'end')
             self.pathInput.insert(0, path)
 
-        def onEncrypt(self, input):
-            path = input.get()
-            print(path)
+        def onEncrypt(self, inputPath, inputKey):
+            path = inputPath.get()
 
 
         def  init_window(self):
@@ -28,23 +30,27 @@ def gui():
             self.pack(fill=BOTH, expand=1)
 
             #tworzenie text input
-            self.pathInput.configure(textvariable=self.input)
+            self.pathInput.configure(textvariable=self.inputPath, width=50)
+            self.pathInput.insert(0, "Ścieżka do pliku")
             self.pathInput.pack()
+
+            self.keyInput.configure(textvariable=self.inputKey, width=50)
 
             #tworzenie buttona. Trzeba dodać comand=encrypt()
             browseButton = Button(self, text="Przeglądaj", command=self.onBrowse)
-            encryptButton = Button(self, text="Encrypt", command=lambda: self.onEncrypt(self.input))
+            encryptButton = Button(self, text="Encrypt", command=lambda: self.onEncrypt(self.inputPath, self.inputKey))
             decryptButton = Button(self, text="Decrypt")
 
-            #umiejscowienie buttona
-            browseButton.place(x=150, y=50)
-            encryptButton.place(x=55, y=100)
-            decryptButton.place(x=135, y=100)
-            self.pathInput.place(x=10, y=55)
+            #umiejscowienie
+            browseButton.place(x=330, y=10)
+            encryptButton.place(x=135, y=100)
+            decryptButton.place(x=215, y=100)
+            self.pathInput.place(x=10, y=15)
+            self.keyInput.place(x=10, y=50)
 
     root = Tk()
     #rozmiar okna
-    root.geometry("220x200")
+    root.geometry("420x200")
 
     app = Window(root)
     root.mainloop()
