26c63ee93aa67160ccb284d7aa9d4440ea79c586
Patryk Konopka <skinex11@gmail.com>
2019-05-20 20:09:32 +0200

    Naprawione odczytywanie kodowania

--- Szyfrator/Decryptor.py
+++ Szyfrator/Decryptor.py
@@ -3,7 +3,13 @@ import os
 from Gui import gui
 
 
-def encrypt(path, key, resultFileName):
+def reverse(string):
+    res = ""
+    for i in string:
+        res = i + string
+    return res
+
+def decrypt(path, key, resultFileName):
     resultTable = []
     contentTable = []
     # konwersja klucza na kody ASCII
@@ -18,7 +24,7 @@ def encrypt(path, key, resultFileName):
 
 # Punkt 6 ----------------------------------------------------------------------------------------------
     #otwieramy plik
-    file = open(path, "r")
+    file = open(path, "r", encoding="ISO-8859-1")
     while True:
         #odczytujemy 5 znaków z pliku
         content = file.read(5)
@@ -31,23 +37,41 @@ def encrypt(path, key, resultFileName):
             file.close()
             break
 
-    #odwracamy szyfrogram
-    contentTable.reverse()
-    for content in contentTable:
-        content.reverse()
+    print(contentTable)
 
-#Punkt 5 --------------------------------------------------------------------------------------------
-    #w kazdym bloku przesuwamy nie parzyste pozycje na poprzednią nieparzystą pozycję
-    for content in contentTable:
-        #dla bloków 5-elementowych zmieniamy 3 pozycje
-        if len(content) == 5:
-            content[4], content[0], content[2] = content[0], content[2], content[4]
-        #dla bloków 3 i 4 elementowych zmieniamy dwie pozycje
-        elif 2 < len(content) < 5:
-            content[2], content[0] = content[0], content[2]
-        #dla bloków 1 i 2 elementowych nie robimy nic
-
-#Punkt 4 -------------------------------------------------------------------------------------------
-    #zamieniamy znaki na kody ASCII
-    for content in contentTable:
-        contentTable[contentTable.index(content)] = [ord(char) for char in content]
+#     for content in contentTable:
+#         contentTable[contentTable.index(content)] = [ord(char) for char in content]
+#
+#     #odwracamy szyfrogram
+#     contentTable.reverse()
+#     for content in contentTable:
+#         content.reverse()
+#
+# #Punkt 5 --------------------------------------------------------------------------------------------
+#     #w kazdym bloku przesuwamy nie parzyste pozycje na poprzednią nieparzystą pozycję
+#     for content in contentTable:
+#         #dla bloków 5-elementowych zmieniamy 3 pozycje
+#         if len(content) == 5:
+#             content[4], content[0], content[2] = content[0], content[2], content[4]
+#         #dla bloków 3 i 4 elementowych zmieniamy dwie pozycje
+#         elif 2 < len(content) < 5:
+#             content[2], content[0] = content[0], content[2]
+#         #dla bloków 1 i 2 elementowych nie robimy nic
+#
+# #Punkt 4 -------------------------------------------------------------------------------------------
+#     #zamieniamy znaki na kody ASCII
+#
+#
+# #Punkt 3 -------------------------------------------------------------------------------------------
+#     for content in contentTable:
+#         result = []
+#         #dla każdego bloku obliczamy różnicę znaku szyfrowanego i klucza
+#         for i in range(0, len(content)):
+#             result.append(content[i]-key[i])
+#         #dodajemy odszyfrowany blok do zbiorczej tablicy
+#         resultTable.append(result)
+#
+#     for result in resultTable:
+#         resultTable[resultTable.index(result)] = [chr(char) for char in result]
+#
+#     print(resultTable)
--- Szyfrator/Encryptor.py
+++ Szyfrator/Encryptor.py
@@ -1,5 +1,6 @@
 import os
 
+from Decryptor import decrypt
 from Gui import gui
 
 
@@ -70,18 +71,21 @@ def encrypt(path, key, resultFileName):
 
     resultFile = open(resultFileName, "w+", encoding="ISO-8859-1")
     for result in resultTable:
+        print(result)
         for i in range(0, len(result)):
             resultFile.write(result[i])
     resultFile.close()
 
     #usuwanie pliku oryginalnego
-    os.remove(path)
+    #os.remove(path)
 
 
 if __name__ == '__main__':
-    while True:
-        path, key, result, choice = gui()
-        if choice:
-            encrypt(path, key, result)
-        else:
-            decrypt(path, key, result)
+    encrypt("D:\OneDrive\PWr\Semestr 6\BIOD\Szyfrator\etest.txt", "xyz-2", "es")
+    decrypt('D:\OneDrive\PWr\Semestr 6\BIOD\Szyfrator\es',"xyz-2","test")
+    # while True:
+    #     path, key, result, choice = gui()
+    #     if choice:
+    #         encrypt(path, key, result)
+    #     else:
+    #         decrypt(path, key, result)
